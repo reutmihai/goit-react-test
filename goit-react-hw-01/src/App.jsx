0d0bd882c5fbd9683c59f4a1e67b4c3928ab9aab
Reut Mihai <reutmihai@icloud.com>
2024-10-01 21:13:35 +0300

    profile component

--- goit-react-hw-01/src/App.jsx
+++ goit-react-hw-01/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Profile from './Profile';
+import user from '/Users/mihaireut/Documents/GitHub/goit-js-project/goit-react-hw-01-components/goit-react-hw-01/user.json';
 
 function App() {
 
   return (
     <>
-    
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </>
-  )
+  );
 }
 
 export default App
