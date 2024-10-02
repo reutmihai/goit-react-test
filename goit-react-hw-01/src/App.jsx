dd5368d4e1a41c7fd1e63e7a759778a5667183b3
Reut Mihai <reutmihai@icloud.com>
2024-10-02 13:22:13 +0300

    upload stats, hw2

--- goit-react-hw-01/src/App.jsx
+++ goit-react-hw-01/src/App.jsx
@@ -1,7 +1,8 @@
 import './App.css'
 import Profile from './Profile';
+import Statistics from './Statistics';
 import user from '/Users/mihaireut/Documents/GitHub/goit-js-project/goit-react-hw-01-components/goit-react-hw-01/user.json';
-
+import data from '/Users/mihaireut/Documents/GitHub/goit-js-project/goit-react-hw-01-components/goit-react-hw-01/data.json';
 function App() {
 
   return (
@@ -13,6 +14,8 @@ function App() {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
     </>
   );
 }
---
+++ goit-react-hw-01/src/Statistics.jsx
@@ -0,0 +1,45 @@
+import PropTypes from 'prop-types';
+import './Statistics.css';
+
+const Statistics = ({ title, stats }) => {
+  return (
+    <section className="statistics">
+      {title && <h2 className="title">{title}</h2>}
+
+      <ul className="stat-list">
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            key={id}
+            className="item"
+            style={{ backgroundColor: getRandomColor() }}
+          >
+            <span className="label">{label}</span>
+            <span className="percentage">{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+const getRandomColor = () => {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Statistics;
